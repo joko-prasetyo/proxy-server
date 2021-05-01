7b3a3c5a58b708f32ca508403e9cd413cd9dc400
Joko Prasetyo <robertkwan.rk@gmail.com>
2021-05-01 18:26:51 +0700

    Test

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ app.use(express.json());
 
 app.post("/", async (req, res) => {
   try {
-    const { headers, body } = req;
+    const { url } = req.body;
     let options = {
-      headers,
+      headers: {
+        "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)",
+        Accept: "*/*",
+        Host: "www.blibli.com",
+        "Content-Type": "application/json",
+      },
       method: "GET",
     };
-    const response = await fetch(body.url, options);
+    const response = await fetch(url, options);
     res.send(await response.json());
   } catch (e) {
     res.status(500).send(e);
