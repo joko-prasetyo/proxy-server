d8a3339f5010780b64207a43daf3bc49454a55be
Joko Prasetyo <robertkwan.rk@gmail.com>
2021-05-02 13:25:28 +0700

    Update

--- index.js
+++ index.js
@@ -4,18 +4,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.post("/", async (req, res) => {
+app.get("/:url", async (req, res) => {
   try {
+    const url = req.params.url;
     const { headers } = req;
-    const { method, url } = req.body;
-    let options = {
-      headers,
-      method,
-    };
-
     delete headers["host"];
 
-    const response = await fetch(url, options);
+    const response = await fetch(url, {
+      headers,
+    });
+
+    res.send(await response.json());
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e.toString());
+  }
+});
+
+app.post("/:url", async (req, res) => {
+  try {
+    const url = req.params.url;
+    const { headers, body } = req;
+    delete headers["host"];
+
+    const response = await fetch(url, {
+      headers,
+      body: JSON.stringify(body),
+      method: "POST"
+    });
+
     res.send(await response.json());
   } catch (e) {
     console.log(e);
--- test.js
+++ test.js
@@ -13,7 +13,7 @@ let options = {
     let merchant_url = "https://www.blibli.com/backend/search/merchant/BLI-60048?page=1&start=0&pickupPointCode=&cnc=&multiCategory=true&excludeProductList=true"
     let product_url = "https://www.blibli.com/backend/product-detail/products/ps--AAT-70000-02836/_summary?defaultItemSku="
     let merchant_fav = "https://www.blibli.com/backend/official/merchant-favorites/BLI-60048"
-    let proxy = "https://scrapper-proxy.herokuapp.com";
+    let proxy = `https://scrapper-proxy-${Math.floor(Math.random() * 15)}.herokuapp.com`;
     console.log(new Date());
     let [response1, response2, response3] = await Promise.all([
         fetch(proxy, { ...options, method: "POST", body: JSON.stringify({ url:merchant_url  }) }, timeout), 
@@ -22,13 +22,5 @@ let options = {
     ]);
 
     let [res1, res2, res3] = await Promise.all([response1.json(), response2.json(), response3.json()]);
-    console.log(new Date());
-
-    response1 = await fetch(proxy, { ...options, method: "POST", body: JSON.stringify({ url:merchant_url  }) }, timeout);
-    res1 = await response1.json();
-    response2 = await fetch(proxy, { ...options, method: "POST", body: JSON.stringify({ url:product_url  }) }, timeout);
-    res2 = await response2.json();
-    response3 = await fetch(proxy, { ...options, method: "POST", body: JSON.stringify({ url:merchant_fav  }) }, timeout);
-    res3 = await response3.json();
     console.log(new Date());
 })();
